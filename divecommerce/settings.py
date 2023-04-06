393b5c1b9f7e2a9cf6d7880e94406435a1998742
Nathaniel Nosa <proncode@gmail.com>
2023-04-06 10:30:21 +0100

    ip added

--- divecommerce/settings.py
+++ divecommerce/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('DUMMY_KEY')) 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.50.226.249','localhost','*','127.0.0.1:8000']
 
 
 # Application definition
